624a7dd011e46b8d7ba1bb9b80b0fec15fe1937c
Shawn Davis <shawn@develmaycare.com>
2018-02-08 21:33:00 -0500

    Added constant locators.

---
+++ htmgel/constants.py
@@ -0,0 +1,16 @@
+import os
+import locator
+
+PATH_TO_HTMGEL = os.path.dirname(os.path.abspath(locator.__file__))
+PATH_TO_HTMGEL_BOOTSTRAP3 = os.path.join(os.path.dirname(PATH_TO_HTMGEL), "htmgel_bootstrap3")
+PATH_TO_HTMGEL_BOOTSTRAP4 = os.path.join(os.path.dirname(PATH_TO_HTMGEL), "htmgel_bootstrap4")
+PATH_TO_HTMGEL_FOUNDATION6 = os.path.join(os.path.dirname(PATH_TO_HTMGEL), "htmgel_foundation6")
+
+if os.path.exists(PATH_TO_HTMGEL_BOOTSTRAP3):
+    HTMGEL_FRAMEWORK = "bootstrap3"
+elif os.path.exists(PATH_TO_HTMGEL_BOOTSTRAP4):
+    HTMGEL_FRAMEWORK = "bootstrap4"
+elif os.path.exists(PATH_TO_HTMGEL_FOUNDATION6):
+    HTMGEL_FRAMEWORK = "foundation6"
+else:
+    HTMGEL_FRAMEWORK = None
---
+++ htmgel/locator.py
@@ -0,0 +1,5 @@
+"""
+This is a placeholder file that allows the installed location of the quicksite package to be reliably found. See
+``constants.py`` for usage.
+
+"""
